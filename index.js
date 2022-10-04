9f5e354d50ddd9bf5103dfd861c865663c0d00f5
Nic <nic.dan.costea@gmail.com>
2022-10-04 15:10:24 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-// To make practice with variables you should create what follows:
+// Variables exercises
 
+// #1. Create an index.js file in a folder and open the folder with VSCode
+// Follow the steps of the recipe and translate it into code.
+// You’re going to create a variable for each step of the recipe, and you’re naming it with a meaningful name representing the recipient used or the step itself.
+// In each variable you’re going to sum the quantities of the ingredients used.
+// In the end you should have a variable containing the sum of all the ingredients in a variable called “carbonara”
+// display the value of carbonara in a console.log()
+
+// Spaghetti alla Carbonara Recipe:
+// Ingredients for 4 persons:
+//          spaghetti 400g,
+//          guanciale 250g,
+//          very fresh egg yolks 6,
+//          aged grated Pecorino Romano cheese 50g,
+//          raw black pepper 4g
+
+// #2. To make practice with variables you should create what follows:
 // - a variable for your name
 let firstName = "Dan";
 
@@ -27,6 +43,6 @@ let currentYear = 2022;
 let birthYear = currentYear - age;
 console.log({ birthYear });
 
-let homework = `${greeting}, I am ${age} years old, and I live in Malta`;
+let homework = `${greeting}, I am ${age} years old, was born in Romania, and currently live in Malta`;
 const text = document.querySelector("h1");
 text.textContent = homework;
