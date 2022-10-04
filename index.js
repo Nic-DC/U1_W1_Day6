b0d37e5523b7de614d07e3cb09785e0354b113b6
Nic <nic.dan.costea@gmail.com>
2022-10-04 16:59:51 +0200

    Finalized Homework
    - with the line 83 [const text = document.querySelector("h1");], node does NOT run

--- index.js
+++ index.js
@@ -2,18 +2,54 @@
 
 // #1. Create an index.js file in a folder and open the folder with VSCode
 // Follow the steps of the recipe and translate it into code.
-// You’re going to create a variable for each step of the recipe, and you’re naming it with a meaningful name representing the recipient used or the step itself.
-// In each variable you’re going to sum the quantities of the ingredients used.
-// In the end you should have a variable containing the sum of all the ingredients in a variable called “carbonara”
-// display the value of carbonara in a console.log()
 
 // Spaghetti alla Carbonara Recipe:
-// Ingredients for 4 persons:
-//          spaghetti 400g,
-//          guanciale 250g,
-//          very fresh egg yolks 6,
-//          aged grated Pecorino Romano cheese 50g,
-//          raw black pepper 4g
+const spaghetti = 400;
+const guanciale = 250;
+const eggYolks = 6;
+const cheesePecorino = 50;
+const blackPepper = 4;
+const water = 2;
+const salt = 1;
+const mediumHeat = 3;
+const highHeat = 1;
+const setAside = 1;
+
+// To make classic carbonara, first cut the guanciale into 1cm layers, then into long strips.
+// Combine the egg yolks with the finely grated Pecorino Romano.
+// Roast the black pepper on a pan until it smokes and sizzles, and combine a small amount of
+// it to the yolks and chees mixture, set aside the rest.
+let stripsGuanciale = guanciale;
+let dimensionlayerGuanciale = 1;
+let yolksAndPecorino = eggYolks + cheesePecorino;
+let quarterRoastedBlackPepper = blackPepper / 4;
+let restOfRoastedBlackPepper = blackPepper - quarterRoastedBlackPepper;
+let yolksPecorinoPepperMix = yolksAndPecorino + quarterRoastedBlackPepper;
+
+// Brown the strips of guanciale for 3 minutes at medium heat, then 1 minune on high heat til crisp,
+// then turn off the heat, take out the guanciale, leaving only the grease on the pan cool down.
+// Cook the pasta with a pinch of salt in the water (guanciale is already very salty);
+// set aside a ladleful of the pasta water before draining it, then drain the pasta once it is cooked al dente.
+let greaseGuanciale = 20;
+let brownStripsGuanciale =
+  stripsGuanciale - greaseGuanciale + highHeat + mediumHeat;
+let cookedSpaghetti = spaghetti + water + salt;
+let drainedSpaghetti = spaghetti + salt + (1 / 20) * water;
+let ladleSpaghettiWater = 1;
+
+// Pour 2 spoons of the reserved hot water into the part of the frying pan containing the cooled guanciale and turn on the heat:
+// this will create a creamy sauce with the pasta starch contained in that water.
+// TURN OFF THE HEAT AND SET ASIDE for 1 minute, then transfer the pasta to the same pan and mix together.
+// Add the yolk and cheese mixture, stirring rapidly. In the warm pan with the hot pasta, the eggs will cook gently and become creamy.
+// It is important to do this part quickly to prevent the yolks from congealing and taking on the texture of scrambled eggs.
+// Add the remaining roasted black pepper and serve immediately.
+let reservedSpaghettiWater = 2;
+let cooledGuanciale = brownStripsGuanciale;
+let creamySauce = reservedSpaghettiWater + cooledGuanciale;
+let sauceAndSpaghettiMix = creamySauce + drainedSpaghetti + setAside;
+let preCarbonara = sauceAndSpaghettiMix + yolksPecorinoPepperMix;
+let carbonara = preCarbonara + restOfRoastedBlackPepper;
+console.log({ carbonara });
 
 // #2. To make practice with variables you should create what follows:
 // - a variable for your name
@@ -44,5 +80,5 @@ let birthYear = currentYear - age;
 console.log({ birthYear });
 
 let homework = `${greeting}, I am ${age} years old, was born in Romania, and currently live in Malta`;
-const text = document.querySelector("h1");
-text.textContent = homework;
+// const text = document.querySelector("h1");
+// text.textContent = homework;
